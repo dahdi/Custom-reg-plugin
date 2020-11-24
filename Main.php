6a149bc0495640d2376ad45527626211d886f0df
dahdi <62443126+dahdi@users.noreply.github.com>
2020-11-24 01:05:54 +0000

    custom reg function created
    This function will use post requests for each attribute to put the other created functions to use

--- Main.php
+++ Main.php
@@ -96,7 +96,7 @@ function registration_validation( $username, $password, $email, $website, $first
     }
 }
 
-// Below is the complete registration function whic will accept an array of user data
+// Below is the complete registration function which will accept an array of user data
 function complete_registration() {
     global $reg_errors, $username, $password, $email, $website, $first_name, $last_name, $nickname, $bio;
     if ( 1 > count($reg_errors->get_error_messages())) {
@@ -116,4 +116,52 @@ function complete_registration() {
 }
 
 // Below is the custom registration function that puts all the function above into use
+function custom_registration_function() {
+    if (isset($_POST['submit'])) {
+        registration_validation(
+            $_POST['username'],
+            $_POST['password'],
+            $_POST['email'],
+            $_POST['website'],
+            $_POST['fname'],
+            $_POST['lname'],
+            $_POST['nickname'],
+            $_POST['bio']
+        );
 
+        //sanatize user form input
+        global $username, $password, $email, $website, $first_name, $last_name, $nickname, $bio;
+        $username   =   sanatize_user($_POST['username']);
+        $password   =   esc_attr($_POST['password']);
+        $email      =   sanatize_email($_POST['email']);
+        $website    =   esc_url( $_POST['website'] );
+        $first_name =   sanitize_text_field( $_POST['fname'] );
+        $last_name  =   sanitize_text_field( $_POST['lname'] );
+        $nickname   =   sanitize_text_field( $_POST['nickname'] );
+        $bio        =   esc_textarea( $_POST['bio'] );
+
+        //call @function complete_registration to create the user
+        // only when no wp error is found
+        complete_registration(
+            $username,
+            $password,
+            $email,
+            $website,
+            $first_name,
+            $last_name,
+            $nickname,
+            $bio
+        );
+    }
+
+    registration_form(
+        $username,
+        $password,
+        $email,
+        $website,
+        $first_name,
+        $last_name,
+        $nickname,
+        $bio
+    );
+}
